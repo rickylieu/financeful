1a53a1a65fdaa596718f2fcf60f226b9c4ccf0b6
Ricky Lieu <r1lieu@ucsd.edu>
2016-11-03 03:17:12 -0700

    Second Try

--- routes/set.js
+++ routes/set.js
@@ -3,5 +3,5 @@ var data = require("../data.json");''
 exports.set = function(req, res) {
 	// Your code goes here
   console.log("yay, set");
-  res.render('set', { data, "add_purchase": "/add_purchase", "view": "/view", "index": "/index"} );
+  res.render('set', {"add_purchase": "/add_purchase", "view": "/view", "index": "/index"} );
 }
