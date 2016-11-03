d59670708bffaaf115e2839bd4883345260d873a
Ricky Lieu <r1lieu@ucsd.edu>
2016-11-03 03:10:47 -0700

    Second Try

--- routes/view.js
+++ routes/view.js
@@ -4,5 +4,5 @@ exports.view = function(req, res) {
 	// Your code goes here
 	console.log(data);
   console.log("yay, view just ran!");
-  res.render('view', data );
+  res.render('view', {  "index": "/index" } );
 }
