f53d6fa3195e3b0c37c83a2a33260c33929a41a8
Ricky Lieu <r1lieu@ucsd.edu>
2016-11-03 20:32:47 -0700

    Second Try

--- public/js/financeful.js
+++ public/js/financeful.js
@@ -1,5 +1,3 @@
-'use strict';
-
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
@@ -9,15 +7,41 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-		console.log("Javascript connected!");
-	$.getJSON('data.json',function(data){
-      console.log(data.purchases[1]);
-      var obj = { item:"Food", price:"10", category:"play"};
-      data.purchases.push(obj);
-      console.log(data.purchases[3]);
-    });
+     console.log("Javascript connected!");
+     $(".category").click(projectClick);
+
+
+    
 }
 
-function projectClick(e) {  
+
+function projectClick(e) { 
+	e.preventDefault();
+
+
+	var text = $(this).text().replace(/\s/g,'');
+	var category = $("#status");
+	var totalCost = 0;
+    
+   $.getJSON('data.json',function(data){
+	var i;
+	var cost = 0;
+	console.log(data.purchases.length);
+	for( i=0; i < data.purchases.length; i++)	{
+		var readText = data.purchases[i].category.toUpperCase();
+		console.log(readText);
+		//console.log(text.toUpperCase());
+		totalCost = totalCost + +data.purchases[i].price;
+	  if(readText === text.toUpperCase()) {
+        cost = +cost + +data.purchases[i].price;
+      } 
+    }
+   console.log(cost);
+   console.log(totalCost);
+   console.log("You've spent " + Math.round(cost / totalCost * 100) + "% on " + text);
+   category.text("You've spent $" + cost + "(" + Math.round(cost / totalCost * 100) + "%) on " + text.toLowerCase() + ".");
+   delete cost;
+
+  })  
 	
 }
